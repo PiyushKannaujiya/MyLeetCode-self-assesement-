8e130ce162b585f8398ff779d19ded2b9ecbd345
PiyushKannaujiya <kannaujiyapiyush87@gmail.com>
2026-03-02 15:33:32 +0530

    778 lc code using dijktra with o(n^2logn) as heap operation take log n^2 and for loop is n^2

--- graph/778_SwinInRisingWater.cpp
+++ graph/778_SwinInRisingWater.cpp
@@ -1,1 +1,44 @@
-    
+#include <bits/stdc++.h>
+using namespace std;
+int swimRising(vector<vector<int>>&grid){
+    int n = grid.size();
+    priority_queue<pair< int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int, pair<int,int>>>>pq;
+    vector<vector<bool>>vis(n,vector<bool>(n,false));
+    vector<vector<int>>dir = {{-1,0},{1,0},{0,1},{0,-1}};
+  pq.push({grid[0][0],{0,0}});
+  while(!pq.empty()){
+    int time = pq.top().first;
+ int x = pq.top().second.first;
+ int y = pq.top().second.second;
+
+
+pq.pop();
+if(x == n-1 && y == n-1) return time;
+ if(vis[x][y]) continue;
+
+ vis[x][y] = true;
+ for(auto &it :dir){
+    int i_ = x + it[0];
+    int j_  = y + it[1];
+    if(i_ >= 0 && j_>=0 && i_< n && j_ < n && !vis[i_][j_] )
+ {
+ int  ntime = max(time,grid[i_][j_]);
+  pq.push({ntime,{i_,j_}});
+ }
+}
+
+
+  }
+  return -1;
+
+}
+
+int main (){
+vector<vector<int>>grid = {
+    {0,1},{2,3}
+};
+int a = swimRising(grid);
+cout << a << endl;
+
+    return 0;
+}
