ac76197cd80a8e43b24ce57fa182407a247cb6d4
PiyushKannaujiya <kannaujiyapiyush87@gmail.com>
2026-03-25 16:28:19 +0530

    already done on gfg

---
+++ heap/converting_min_to_max_heap.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void maxheap(vector<int> &arr,int n,int i){
+    int largest =i;
+    int left = 2*i +1;
+    int right = 2*i+2;
+    if(left <n && arr[largest]< arr[left]){
+        largest = left;
+    }
+     if(right <n && arr[largest]< arr[right]){
+        largest = right;
+    }
+  if(largest != i){
+    swap(arr[largest],arr[i]);
+    maxheap(arr,n,largest);
+  }
+
+}
+
+
+int main(){
+      vector<int> arr = {1, 3, 5, 7, 9, 6};
+     int n = arr.size();
+     for(int i = n/2-1;i>= 0;i--){
+        maxheap(arr,n,i);
+     }
+     for(auto a :arr){
+        cout << a << " ";
+     }
+     cout << endl;
+      return 0;
+
+
+}
